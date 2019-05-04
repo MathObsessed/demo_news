62561e5936fac59c163ab857478a236f914164fd
Ernest Lebedev <elebedev@futuriti.org>
2019-05-05 02:52:43 +0300

    Added some Twig views and news actions

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,16 +2,41 @@
 
 namespace App\Controller;
 
+use App\Service\NewsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController {
+    private $newsService;
+
+    public function __construct(NewsService $newsService) {
+        $this->newsService = $newsService;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function index() {
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController'
+            'news' => $this->newsService->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/news/{id<\d+>}", name="news_show")
+     */
+    public function newsShow($id) {
+        return $this->render('default/news.html.twig', [
+            'item' => $this->newsService->findById($id)
+        ]);
+    }
+
+    /**
+     * @Route("/news/delete/{id<\d+>}", name="news_delete")
+     */
+    public function newsDelete($id) {
+        //TODO: Implement delete logic!
+
+        return $this->redirectToRoute('homepage');
     }
 }
