263610224bcd4d0bb5b53a0f80314a9b9ea36560
Ernest Lebedev <elebedev@futuriti.org>
2019-05-04 22:28:58 +0300

    Forgot to add "fixtures" bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,6 +12,7 @@ return [
 
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
