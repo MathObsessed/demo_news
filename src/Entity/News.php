a357cf8a9ee8bebed48cc408fbd2db50d647d32e
Ernest Lebedev <elebedev@futuriti.org>
2019-05-05 20:59:48 +0300

    Added Entity validation constraints

--- src/Entity/News.php
+++ src/Entity/News.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\SoftDeleteable\Traits\SoftDeleteableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NewsRepository")
@@ -27,26 +28,45 @@ class News {
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     *
+     * Regexp list allows:
+     *      - Unicode characters range а-я
+     *      - Unicode character ё
+     *      - Space
+     *      - Any kind of punctuation character
+     *      - Letters range a-z
+     *      - Numbers range 0-9
+     * Modifiers:
+     *      - i - insensitive
+     *      - u - unicode
+     * @Assert\Regex(pattern="/^[\x{0430}-\x{044F}\x{0451} \p{P}a-z0-9]+$/iu")
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Image(minWidth = 130, maxWidth = 130, minHeight = 100, maxHeight = 100)
      */
     private $thumbnail;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Image(minWidth = 800, maxWidth = 800)
      */
     private $image;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Url(protocols = {"http", "https"}, relativeProtocol = false)
      */
     private $url;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $text;
 
