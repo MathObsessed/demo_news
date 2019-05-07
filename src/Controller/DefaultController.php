e15980a84eb99da525296c538d2cb68682895333
Ernest Lebedev <elebedev@futuriti.org>
2019-05-07 16:02:12 +0300

    Updated views to use REST API on POST, PUT and DELETE actionsm also removed recundant code from DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\News;
 use App\Exception\NewsNotFound;
-use App\Form\NewsType;
 use App\Service\AssetService;
 use App\Service\NewsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\HeaderUtils;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController {
@@ -92,105 +87,5 @@ class DefaultController extends AbstractController {
         $response->headers->set('Content-Disposition', HeaderUtils::makeDisposition(HeaderUtils::DISPOSITION_INLINE, $fileName));
 
         return $response;
-    }
-
-    /**
-     * @Route("/news/{id<\d+>}", methods={"DELETE"}, name="delete_news_by_id")
-     */
-    public function newsDelete($id) {
-        try {
-            $this->newsService->deleteById($id);
-        }
-        catch (NewsNotFound $e) {
-            throw $this->createNotFoundException($e->getMessage());
-        }
-
-        return new Response('', Response::HTTP_OK);
-    }
-
-    /**
-     * @Route("/news", methods={"POST"}, name="post_news")
-     */
-    public function newsPost(Request $request) {
-        $now = new \DateTimeImmutable();
-
-        $form = $this->createForm(NewsType::class);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var News $item */
-            $item = $form->getData();
-
-            $item->setCreated($now);
-            $item->setThumbnail($this->assetService->handleUpload($form->get(NewsType::THUMBNAIL)->getData()));
-            $item->setImage($this->assetService->handleUpload($form->get(NewsType::IMAGE)->getData()));
-
-            $this->newsService->persist($item);
-
-            return new Response('', Response::HTTP_OK);
-        }
-
-        return $this->json($this->extractErrorsFromForm($form), Response::HTTP_BAD_REQUEST);
-    }
-
-    /**
-     * @Route("/news/{id<\d+>}", methods={"POST"}, name="put_news_by_id")
-     */
-    public function newsPut(Request $request, $id) {
-        try {
-            $item = $this->newsService->findById($id);
-
-            $thumbnailValue = $item->getThumbnail();
-            $imageValue = $item->getImage();
-
-            $item->setThumbnail($this->assetService->fileByName($thumbnailValue));
-            $item->setImage($this->assetService->fileByName($imageValue));
-        }
-        catch (NewsNotFound $e) {
-            throw $this->createNotFoundException($e->getMessage());
-        }
-
-        $form = $this->createForm(NewsType::class, $item);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($uploadedThumbnail = $form->get(NewsType::THUMBNAIL)->getData()) {
-                $thumbnailValue = $this->assetService->handleUpload($uploadedThumbnail);
-            }
-            if ($uploadedImage = $form->get(NewsType::IMAGE)->getData()) {
-                $imageValue = $this->assetService->handleUpload($uploadedImage);
-            }
-
-            $item->setThumbnail($thumbnailValue);
-            $item->setImage($imageValue);
-
-            $this->newsService->persist($item);
-
-            return new Response('', Response::HTTP_OK);
-        }
-
-        return $this->json($this->extractErrorsFromForm($form), Response::HTTP_BAD_REQUEST);
-    }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private function extractErrorsFromForm(FormInterface $form):array {
-        $errors = [];
-
-        foreach ($form->getErrors() as $error) {
-            $errors[] = $error->getMessage();
-        }
-
-        foreach ($form->all() as $childForm) {
-            if ($childForm instanceof FormInterface) {
-                if ($childErrors = $this->extractErrorsFromForm($childForm)) {
-                    $errors[$childForm->getName()] = $childErrors;
-                }
-            }
-        }
-
-        return $errors;
     }
 }
