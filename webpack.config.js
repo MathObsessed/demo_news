eb5409147c4b6ef198825c76a7c71f29cc5d6c02
Ernest Lebedev <elebedev@futuriti.org>
2019-05-07 21:26:41 +0300

    Installed NPM dependencies

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+
+    .splitEntryChunks()
+
+    .cleanupOutputBeforeBuild()
+    .disableSingleRuntimeChunk()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel(() => {}, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    });
+
+module.exports = Encore.getWebpackConfig();
